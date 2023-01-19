e923170d933257711b9c477d83bbb6110b321384
Diana Eboh <dianaeboh@dianas-air.home>
2023-01-19 16:00:15 +0000

    added initial comments

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Create a simple number guessing game, that takes in some user input matches it to a 'secret' number.
+        // Prompt user to enter number
+        System.out.println();
+        // Collect user input
+        // if number is 22 print 'woohoo'
+        // otherwise print 'womp womp'
     }
 }
